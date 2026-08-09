f496f6f9ddafc2d2a5691c47526f41f9114ea2ae
Mandelbrot <aokoroko@gmail.com>
2026-08-09 17:46:21 +0300

    Add

--- main.cpp
+++ main.cpp
@@ -172,8 +172,8 @@ int main() {
 
     double limit_epsilon_squared;
     mpfr_exp_t current_binary_exp = mpfr_get_exp(st);
-    if (current_binary_exp > -70) {
-        limit_epsilon_squared = 1e-20;
+    if (current_binary_exp > -244) {
+        limit_epsilon_squared = 1e-40;
     } else {
         limit_epsilon_squared = 1e-60;
     }
